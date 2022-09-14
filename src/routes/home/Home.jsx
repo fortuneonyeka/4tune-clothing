3abe975ef677b337c4db4f002fd7e60d8430131d
Ihedoro Fortunatus O <ihedfortune@gmail.com>
2022-09-14 12:25:28 +0100

    update home component

--- src/routes/home/Home.jsx
+++ src/routes/home/Home.jsx
@@ -2,9 +2,11 @@ import React from 'react'
 import Directory from "../../Components/directory/Directory";
 import categories from "../../Data/categories-data";
 
+
 const Home = () => {
   return (
     <div>
+    
       <Directory  categories={categories}/>
     </div>
   )
