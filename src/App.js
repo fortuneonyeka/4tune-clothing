631d2802234d5f0c2137e92786636ade90ce3621
Ihedoro Fortunatus O <ihedfortune@gmail.com>
2022-09-14 12:24:45 +0100

    add the link routes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,33 @@
 import "./App.css";
 import Home from "./routes/home/Home";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route} from "react-router-dom";
+import Navigation from "./routes/navigation/Navigation";
+import Shop from "./routes/shop/Shop";
+import { Contact } from "./routes/contact/Contact";
+import SignIn from "./routes/signIn/SignIn";
+import { Fragment } from "react";
+
+
+
+
+
+
 
 const App = () => {
   return (
-    <div>
+    <Fragment>
       <Routes>
-        <Route path="/" element={<Home />}/>
+        <Route path="/" element={<Navigation/>}>
+        <Route index element={<Home />}/>
+        <Route path="/shop" element={<Shop />}/>
+        <Route path="/contact" element={<Contact />}/>
+        <Route path="/login" element={<SignIn />}/>
+        
+        </Route>
+       
       </Routes>
      
-    </div>
+    </Fragment>
   );
 };
 
