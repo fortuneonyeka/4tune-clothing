b654f865fe376a2ef3592cf8d6e20c1f4e52c0ec
Ihedoro Fortunatus O <ihedfortune@gmail.com>
2022-09-15 16:24:46 +0100

    add utils directory

---
+++ src/utils/firebase/Firebase.utils.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app"
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC120w1k0WLbBu8oKAxjk1xRQz40l-sITA",
+  authDomain: "tune-clothing.firebaseapp.com",
+  projectId: "tune-clothing",
+  storageBucket: "tune-clothing.appspot.com",
+  messagingSenderId: "294108505186",
+  appId: "1:294108505186:web:13fc7e50a89794e4619d3b"
+};
+
+
+const firbaseApp = initializeApp(firebaseConfig);
