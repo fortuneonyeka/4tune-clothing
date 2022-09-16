9723a50599cb39ed52f3ed7587ce749f6a8fdeb1
Ihedoro Fortunatus O <ihedfortune@gmail.com>
2022-09-16 10:49:18 +0100

    build firebase signIn method with google popup

--- src/routes/signIn/SignIn.jsx
+++ src/routes/signIn/SignIn.jsx
@@ -1,10 +1,16 @@
 import React, { Fragment } from 'react'
+import {signInWithGooglePopup} from "../../utils/firebase/Firebase.utils"
 
 const SignIn = () => {
+  const logGoogleUser =  async () => {
+    const response = await signInWithGooglePopup()
+    console.log(response);
+  }
   return (
     <Fragment>
       <div>
         <h3>Sign In</h3>
+        <button onClick={logGoogleUser}>Sign in with Google Popup</button>
       </div>
     </Fragment>
   )
