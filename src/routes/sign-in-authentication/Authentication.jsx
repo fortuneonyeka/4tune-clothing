49281bfcdcb72bff4d0be16d50dc94c869c79465
Ihedoro Fortunatus O <ihedfortune@gmail.com>
2022-09-26 13:17:25 +0100

    complete form styling

--- src/routes/sign-in-authentication/Authentication.jsx
+++ src/routes/sign-in-authentication/Authentication.jsx
@@ -1,37 +1,15 @@
-import React, {  useState } from 'react'
-// import FormInput from "../../Components/form-inputs/Form-input"
-import {signInWithGooglePopup, createUserDocumentFromAuth} from "../../utils/firebase/Firebase.utils"
-
-// import { getRedirectResult } from "firebase/auth"
-// import {signInWithGoogleRedirect, auth} from "../../utils/firebase/Firebase.utils"
+import React from 'react'
 import SignUpForm from "../../Components/sign-up-form/SignUpForm"
 import { Helmet } from "react-helmet-async"
-
 import SignInForm from "../../Components/sign-in/SignInForm"
+import "./authentication.styles.scss"
 
 
 
 
 const Authentication = () => {
-
-
-  // useEffect( () => {
-  //   async function signInWithRedirect() {
-  //     const response = await getRedirectResult(auth)
-  //     if (response) {
-  //       console.log(response);
-  //       const userDocRef = await createUserDocumentFromAuth(response.user)
-  //     }
-  //   }
-  //   signInWithRedirect()
-  // }, []);
-
-
-    
-    
-  return (
-    <div className="">
-      
+    return (
+    <div className="authentication-container">
       <Helmet>
         <title>Login</title>
         <meta name="description" content="User Login page."/>
