545d32406c920dba614d8542487bf41fee233b1f
Ihedoro Fortunatus O <ihedfortune@gmail.com>
2022-10-04 14:17:50 +0100

    user context

--- src/routes/navigation/Navigation.jsx
+++ src/routes/navigation/Navigation.jsx
@@ -1,6 +1,7 @@
 import React, { Fragment, useContext } from "react";
 import { Outlet, Link } from "react-router-dom";
 import { UserContext } from "../../contexts/User.context";
+import {signOutUser} from "../../utils/firebase/Firebase.utils"
 import Logo from "../../Assets/images/4tune-logo.webp";
 import Cart from "../../Assets/images/cart.webp";
 
@@ -8,8 +9,13 @@ import Cart from "../../Assets/images/cart.webp";
 import "./navigation.styles.scss"
 
 const Navigation = () => {
-  const {currentUser} = useContext(UserContext)
-  console.log(currentUser);
+  const {currentUser, setCurrentUser} = useContext(UserContext)
+  
+  const signOutHandler = async () => {
+   await signOutUser()
+   setCurrentUser(null)
+   
+  }
   return (
     <Fragment>
       <div className="navigation">
@@ -29,7 +35,7 @@ const Navigation = () => {
           <Link className="nav-link" to="/contact" >
             Contact
           </Link>
-          {currentUser ? (<span className="nav-link">Sign Out</span>) : (<Link className="nav-link" to="/auth">
+          {currentUser ? (<span className="nav-link" onClick={signOutHandler}>Sign Out</span>) : (<Link className="nav-link" to="/auth">
             SignIn
           </Link>)}
         </div>
